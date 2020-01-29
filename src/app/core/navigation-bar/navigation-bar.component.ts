b8d106046e77583ee1a6a41ccfd21dfa4c3c176e
Romain Billot <romainbillot3009@gmail.com>
2020-01-29 20:23:48 +0100

    wip(responsive)

--- src/app/core/navigation-bar/navigation-bar.component.ts
+++ src/app/core/navigation-bar/navigation-bar.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import {Router} from '@angular/router';
 
 @Component({
@@ -8,6 +8,7 @@ import {Router} from '@angular/router';
 })
 export class NavigationBarComponent implements OnInit {
   @Output() toggleSidenav = new EventEmitter<void>();
+  @Input() routes: Array<any> = [];
 
   private returnUrl = '/';
 
